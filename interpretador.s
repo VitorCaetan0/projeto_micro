9f0733d47db27dd2ba2067ace46ee6b4655ae5e4
vitor <vitor.caetano@unesp.br>
2023-10-26 21:21:30 -0300

    switch

--- interpretador.s
+++ interpretador.s
@@ -7,6 +7,11 @@ movia r6, 0x10001000 /* JTAG UART base address */
 /* print a text string */
 movia r8, TEXT_STRING
 
+/*registradores */
+movia r9, 0x0030
+movia r10, 0x0031    
+movia r11, 0x0032
+
 LOOP:
 ldb r5, 0(r8)
 beq r5, zero, GET_JTAG /* string is null-terminated */
@@ -32,6 +37,12 @@ andhi r4, r4, 0xffff /* check for write space */
 beq r4, r0, END_PUT /* if no space, ignore the character */
 stwio r5, 0(r6) /* send the character */
 END_PUT:
+
+SWITCH:
+beq r5, r9, LEDS
+beq r5,r10, TRIANG
+beq r5,r11, CRONO
+
 /* restore registers */
 ldw r4, 0(sp)
 addi sp, sp, 4
